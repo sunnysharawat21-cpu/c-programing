89eeaa8fea33067444678595909f62a0b53cd799
sunnysharawat21-cpu <sunnysharawat21@gmail.com>
2026-06-15 11:34:43 +0530

    Add files via upload

---
+++ for loop.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+for(int i=0,j =0;i<6,j<50;i++,j++){
+    printf("%d %d\n",i,j);
+    
+}
+    return 0;
+}
