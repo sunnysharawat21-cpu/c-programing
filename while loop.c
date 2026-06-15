9584b6f2463d884f01b56d91ebc8495c239261c4
sunnysharawat21-cpu <sunnysharawat21@gmail.com>
2026-06-15 11:16:54 +0530

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int i=0;
+int main()
+{
+    while(i<30){
+          i++;
+        printf("Hello World %d \n",i);
+      
+    }
+    
+
+    return 0;
+}
